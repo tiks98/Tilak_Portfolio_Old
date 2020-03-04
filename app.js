a5b337807c2d3a96705d2615d45f847413afdcb5
Tilak Suthar <44789847+tiks98@users.noreply.github.com>
2020-03-03 19:58:56 -0500

    Major Commit
    Changed Navbar and deleted unnecessary photos and also added some css!

--- app.js
+++ app.js
@@ -5,86 +5,90 @@
     This is the app javascript file which is created by the express generator while creating the view templates for the website. This file is attached with the node js command prompt.
     References for the file: https://github.com/Naismith/comp2068-f2019-week08/blob/master/app.js
 */
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var flash = require('connect-flash');
-var User = require('./models/user');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var flash = require("connect-flash");
+var User = require("./models/user");
 
 var app = express();
 
 mongoose.connect(
-  'mongodb+srv://test:test@cluster0-dp7bd.mongodb.net/test?retryWrites=true&w=majority',
-  {useNewUrlParser: true, useUnifiedTopology: true}    
+  "mongodb+srv://test:test@cluster0-dp7bd.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 var db = mongoose.connection;
 
-db.on('error', () => console.log('There was a error connecting to MongoDB'));
-db.once('open', () => console.log('We have connected to MongoDB'));
+db.on("error", () => console.log("There was a error connecting to MongoDB"));
+db.once("open", () => console.log("We have connected to MongoDB"));
 
-var indexRouter = require('./routes/index');
-var authRouter = require('./routes/auth');
-
+var indexRouter = require("./routes/index");
+var authRouter = require("./routes/auth");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-
-app.use((req, res, next) => {               //res.locals using the input to display it on the required pages
-  res.locals.title_index = 'Tilak Suthar';
+app.use((req, res, next) => {
+  //res.locals using the input to display it on the required pages
+  res.locals.title_index = "Tilak Suthar";
   next();
 });
 
-app.use((req, res, next) => {             //res.locals using the input to display it on the required pages
-  res.locals.title_projects = 'Projects';
+app.use((req, res, next) => {
+  //res.locals using the input to display it on the required pages
+  res.locals.title_projects = "Projects";
   next();
 });
 
-app.use((req, res, next) => {           //res.locals using the input to display it on the required pages
-  res.locals.title_contact = 'Contact';
+app.use((req, res, next) => {
+  //res.locals using the input to display it on the required pages
+  res.locals.title_contact = "Contact";
   next();
 });
 
-app.use((req, res, next) => {           //res.locals using the input to display it on the required pages
-  res.locals.title_services = 'Services';
+app.use((req, res, next) => {
+  //res.locals using the input to display it on the required pages
+  res.locals.title_services = "Services";
   next();
 });
 
-app.use((req, res, next) => {             //res.locals using the input to display it on the required pages
-  res.locals.title_about = 'About Me';
+app.use((req, res, next) => {
+  //res.locals using the input to display it on the required pages
+  res.locals.title_about = "About Me";
   next();
 });
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(                    // using express session for the application 
-  session({ 
-  secret: 'important', 
-  resave: false, 
-  saveUninitialized: true })
+app.use(
+  // using express session for the application
+  session({
+    secret: "important",
+    resave: false,
+    saveUninitialized: true
+  })
 );
 
-app.use(flash());         // using flash for the application
+app.use(flash()); // using flash for the application
 
-app.use(passport.initialize());  //intializing passport for the app
-app.use(passport.session());     //initalizing session for the passport js
+app.use(passport.initialize()); //intializing passport for the app
+app.use(passport.session()); //initalizing session for the passport js
 
-passport.use(new LocalStrategy(User.authenticate()));  //using local strategy for user authentication
-passport.serializeUser(User.serializeUser());         //serializing user through passport js
-passport.deserializeUser(User.deserializeUser());     //deserializing user through passport js
+passport.use(new LocalStrategy(User.authenticate())); //using local strategy for user authentication
+passport.serializeUser(User.serializeUser()); //serializing user through passport js
+passport.deserializeUser(User.deserializeUser()); //deserializing user through passport js
 
 //Logging into session and authentication
 app.use((req, res, next) => {
@@ -100,10 +104,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', authRouter); //routing it to authRouter
-app.use('/', indexRouter); //routing it to indexRouter
-app.use('/users', require('./routes/users')); //routing it to userRouter
-
+app.use("/", authRouter); //routing it to authRouter
+app.use("/", indexRouter); //routing it to indexRouter
+app.use("/users", require("./routes/users")); //routing it to userRouter
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -114,11 +117,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,33 +4,32 @@
     Website name: https://tilaksuthar.herokuapp.com/
     This is the index javascript file which is used as default javascript file for the website. fetching the necessary data from app.js file
 */
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');  //rendering the index file by putting in express router.
+router.get("/", function(req, res, next) {
+  res.render("index"); //rendering the index file by putting in express router.
 });
 
 /* GET contact page. */
-router.get('/contact', function(req, res, next) {
-  res.render('contact');  //rendering the index file by putting in express router.
+router.get("/contact", function(req, res, next) {
+  res.render("contact"); //rendering the index file by putting in express router.
 });
 
 /* GET aboutme page. */
-router.get('/about_me', function(req, res, next) {
-  res.render('about_me'); //rendering the index file by putting in express router.
+router.get("/about_me", function(req, res, next) {
+  res.render("about_me"); //rendering the index file by putting in express router.
 });
 
 /* GET projects page. */
-router.get('/project', function(req, res, next) {
-  res.render('projects'); //rendering the index file by putting in express router.
+router.get("/project", function(req, res, next) {
+  res.render("projects"); //rendering the index file by putting in express router.
 });
 
 /* GET services page. */
-router.get('/services', function(req, res, next) {
-  res.render('services'); //rendering the index file by putting in express router.
+router.get("/services", function(req, res, next) {
+  res.render("services"); //rendering the index file by putting in express router.
 });
 
 module.exports = router;
